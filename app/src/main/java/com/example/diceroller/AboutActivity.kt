7d0a1bc3944700b5d0e5237a69455d7f1e674af1
Tino Kallinich <tkallinich-friedrich@inovex.de>
2021-02-09 12:39:30 +0100

    L2: refac data binding to data class

--- app/src/main/java/com/example/diceroller/AboutActivity.kt
+++ app/src/main/java/com/example/diceroller/AboutActivity.kt
@@ -13,19 +13,27 @@ class AboutActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAboutBinding
     private lateinit var imm: InputMethodManager
 
+    private val myName: MyName = MyName(nick = "John")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_about)
         imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
+        binding.myName = myName
+
         binding.apply {
             doneButton.setOnClickListener {
-                nameText.text = binding.nicknameEdit.text
+                nameText.text = nicknameEdit.text
+                invalidateAll()
                 nicknameEdit.text.clear()
-                nameText.visibility = View.VISIBLE
                 imm.hideSoftInputFromWindow(nameText.windowToken, 0)
             }
         }
     }
 }
+
+data class MyName(
+    var nick:String = ""
+)
