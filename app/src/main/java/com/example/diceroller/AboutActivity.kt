a76cc6aeb1d233e87d73109c310336c70e92b8b7
Tino Kallinich <tkallinich-friedrich@inovex.de>
2021-02-09 12:22:01 +0100

    L2: added data binding

--- app/src/main/java/com/example/diceroller/AboutActivity.kt
+++ app/src/main/java/com/example/diceroller/AboutActivity.kt
@@ -1,19 +1,31 @@
 package com.example.diceroller
 
-import android.media.Image
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.random.Random
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.example.diceroller.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityAboutBinding
+    private lateinit var imm: InputMethodManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_about)
+        imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        binding.apply {
+            doneButton.setOnClickListener {
+                nameText.text = binding.nicknameEdit.text
+                nicknameEdit.text.clear()
+                nameText.visibility = View.VISIBLE
+                imm.hideSoftInputFromWindow(nameText.windowToken, 0)
+            }
+        }
     }
 }
