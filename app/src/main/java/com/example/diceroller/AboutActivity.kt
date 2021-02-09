f97412c8fdb9a845f0df151ebdb434eb5bd9c94e
Tino Kallinich <tkallinich-friedrich@inovex.de>
2021-02-09 11:07:49 +0100

    L2: begin about me app

---
+++ app/src/main/java/com/example/diceroller/AboutActivity.kt
@@ -0,0 +1,19 @@
+package com.example.diceroller
+
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
